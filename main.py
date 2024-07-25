31aba5af8477484b5adf83bc1f1fae565aa3808e
AsavariD <asavari7d@gmail.com>
2024-07-25 22:04:08 +0530

    question answer functionality

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from langchain_community.document_loaders import PyPDFLoader
-import logging
+from langchain_openai import ChatOpenAI
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+import logging, os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_KEY")
+llm_model = ChatOpenAI(model="gpt-3.5-turbo")
 
 file_path = "../PIC18F45880_user_manual.compressed.pdf"
 loader = PyPDFLoader(file_path)
@@ -9,3 +21,34 @@ loader = PyPDFLoader(file_path)
 pdf_pages_list = loader.load()
 
 logging.info(f"Number of pages in pdf: {len(pdf_pages_list)}")
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(pdf_pages_list)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever()
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+
+question_answer_chain = create_stuff_documents_chain(llm_model, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+results = rag_chain.invoke({"input": "What is ADC in PIC?"})
+
+logging.info(results["context"][0].metadata)
+logging.info(results["answer"])
