5b69a2674148ad349bd7f13025e454bbe62f556f
AsavariD <asavari7d@gmail.com>
2024-07-29 18:40:53 +0530

    CLI input through fire

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 import logging, os
+import fire
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,40 +15,45 @@ logging.basicConfig(level=logging.INFO)
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_KEY")
 llm_model = ChatOpenAI(model="gpt-3.5-turbo")
 
-file_path = "../PIC18F45880_user_manual.compressed.pdf"
-loader = PyPDFLoader(file_path)
 
-pdf_pages_list = loader.load()
+def main(filename, question):
+    file_path = f"../{filename}.pdf"
+    loader = PyPDFLoader(file_path)
 
-logging.info(f"Number of pages in pdf: {len(pdf_pages_list)}")
+    pdf_pages_list = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(pdf_pages_list)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    logging.info(f"Number of pages in pdf: {len(pdf_pages_list)}")
 
-retriever = vectorstore.as_retriever()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(pdf_pages_list)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know."
-    "\n\n"
-    "{context}"
-)
+    retriever = vectorstore.as_retriever()
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know."
+        "\n\n"
+        "{context}"
+    )
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm_model, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+    results = rag_chain.invoke({"input": question})
+
+    logging.info(results["context"][0].metadata)
+    logging.info(results["answer"])
 
 
-question_answer_chain = create_stuff_documents_chain(llm_model, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-results = rag_chain.invoke({"input": "What is ADC in PIC?"})
-
-logging.info(results["context"][0].metadata)
-logging.info(results["answer"])
+if __name__ == "__main__":
+    fire.Fire(main)
